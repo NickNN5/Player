44adba80169ee408c82bfcb1d8c453f3d207d557
unknown <nicknn18@hotmail.com>
2017-09-29 11:42:04 +0200

    se carga lista de canciones mediante js.

--- JS/player.js
+++ JS/player.js
@@ -6,101 +6,117 @@
 
 
 class Player{
-    
-    constructor(album, currentSong){
-        this.album=album;
-        this.currentSong=currentSong;
-        this.queue=loadSongs(album.songList);
-    }
-    
-    loadSongs(songList){
-        var playList=new [];
-        for(var i=0;i<songList.length;i++){
-            playList[i]=songList[i];
-        }
-        return playList;
-    }
-    
-    getFirsSong(){
-        return this.album.getFirstSong();
-    }
-    
-    isLastSong(){
-        return (this.currentSong === this.albumgetLastSong());
-    }
-    
-    setCurrentSong(currentSong){
-        
-        this.currentSong=currentSong;
-        
-        //ponemos el titulo en la barra de reproducción. ( si no lo está) ??
-    }
-    
-    play(){
-        if(this.currentSong!==null){
-            
-            //ponemos el titulo en la barra de reproducción. ( si no lo está) ??
-            
-            //activamos el script para el tiempo. ( resteamos el actual)
-        }
-    }
-    
-    pause(){
-        
-        if(this.currentSong!==null){
-            //cambiamos el icono.
-            
-            //paramos el tiempo
-        }
-    }
-    
-    
-    
-    
+
+    constructor(album, currentSong){
+        this.album=album;
+        this.currentSong=currentSong;
+        this.queue=this.loadSongs(album.songList);
+    }
+
+    loadSongs(songList){
+        var playList=[];;
+        for(var i=0;i<songList.length;i++){
+            playList[i]=songList[i];
+        }
+        return playList;
+    };
+
+    getFirstSong(){
+        return this.album.getFirstSong();
+    };
+
+    isLastSong(){
+        return (this.currentSong === this.albumgetLastSong());
+    }
+
+    setCurrentSong(currentSong){
+
+        this.currentSong=currentSong;
+
+        //ponemos el titulo en la barra de reproducción. ( si no lo está) ??
+    }
+
+    play(){
+        if(this.currentSong!==null){
+
+            //ponemos el titulo en la barra de reproducción. ( si no lo está) ??
+
+            //activamos el script para el tiempo. ( resteamos el actual)
+        }
+    }
+
+    pause(){
+
+        if(this.currentSong!==null){
+            //cambiamos el icono.
+
+            //paramos el tiempo
+        }
+    }
+
+
+
+
 }
 
 class Song{
-    constructor(name, interpreter, duration){
-        this.name=name;
-        this.interpreter=interpreter;
-        tis.duration=duration;
-    }
+    constructor(name, interpreter, duration){
+        this.name=name;
+        this.interpreter=interpreter;
+        this.duration=duration;
+    }
 }
 
 
 class Album{
-    contructor(name, artist, cover, songList){
-        this.name=name;
-        this.artist=artist;
-        this.cover =cover;
-        this.songList=songList;
-    }
-    
-    getLastSong(){
-        return this.songList[this.songList.lenth-1];
-    }
-    
-    getFirstSong(){
-        return this.songList[0];
-    }
-       
-    
-    getAlbumSongIndex(song){
-        for(var i=0;i<this.songList.length;i++){
-            if(song.title===this.songList[i].title){
-                return i;
-            }
-        }
-    }
+    contructor(name, artist, cover, songList){
+        this.name=name;
+        this.artist=artist;
+        this.cover =cover;
+        this.songList=songList;
+    }
+
+    getLastSong(){
+        return this.songList[this.songList.lenth-1];
+    }
+
+    getFirstSong(){
+        return this.songList[0];
+    }
+
+
+    getAlbumSongIndex(song){
+        for(var i=0;i<this.songList.length;i++){
+            if(song.title===this.songList[i].title){
+                return i;
+            }
+        }
+    }
 
 }
 
+function  showQueue(songQueue){
+  //  Element songQueueElement=document.getElementById("song-queue");
+    for(var i=0; i<songQueue.length;i++){
+
+        var songItem=document.createElement("div");
+        songItem.innerHTML="<div class='song-item'>" +
+                "<a> " + songQueue[i].name+ "</a>" +
+                "<a id='totalTime' class='aling-rigth'>"+ songQueue[i].duration + "</a>"+
+                "<br>"+
+                "<small>"+ songQueue[i].interpreter+ "</small> </div>";
+        var e=document.getElementById("song-queue");
+        document.querySelector('#song-queue').appendChild(songItem);
+
+    }
+
+}
 
 //ejecución principal.
-//creamos la lista de canciones:
 var albumT=album;
-//tenemos el album
-alert(albumT.name);
-alert(albumT.artist);
-//eliminar alertas y crear objeto player.
+var mPlayer=new Player(albumT, albumT.getFirstSong);
+//window.onload= showQueue(mPlayer.queue);
 
+window.onload = function() {
+  showQueue(mPlayer.queue);
+};
